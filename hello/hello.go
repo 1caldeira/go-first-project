bf833ff8754480f7d007012acf929c17bf2d05f3
Gabriel Caldeira <g.caldeiraa1@gmail.com>
2024-06-17 13:58:21 -0300

    Update hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -2,39 +2,38 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
+	for {
+		versao := 1.0
+		nome := "Gabriel"
+		fmt.Println("Olá sr.", nome)
+		fmt.Println("Este programa esta na versao", versao)
+		exibeMenu()
+		entrada := digitarEntrada()
 
-	exibeMenu()
-	entrada := digitarEntrada()
+		switch entrada {
+		case 1:
+			iniciarMonitoramento()
 
-	switch entrada {
-	case 1:
-		iniciarMonitoramento()
-
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Nao reconheco esse comando!")
-		os.Exit(-1)
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Nao reconheco esse comando!")
+			os.Exit(-1)
+		}
 	}
 }
-
-func monitorar() {}
 
 func exibirLog() {}
 
 func exibeMenu() {
-	versao := 1.0
-	nome := "Gabriel"
-
-	fmt.Println("Olá sr.", nome)
-	fmt.Println("Este programa esta na versao:", versao)
 
 	fmt.Println("1- Iniciar monitoramento")
 	fmt.Println("2- Exibir logs")
@@ -49,7 +48,20 @@ func digitarEntrada() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	site := "https://www.alura.com.br"
-	//	resp, _ = http.Get(site)
+	sites := []string{"https://httpbin.org/status/200", "https://www.alura.com.br",
+		"https://httpbin.org/status/404", "https://www.youtube.com"}
 
+	for i, site := range sites {
+		fmt.Println("Testando site", i, ":", site)
+		testaSite(site)
+	}
+}
+
+func testaSite(site string) {
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site", site, "esta com problemas! Status Code:", resp.StatusCode)
+	}
 }
